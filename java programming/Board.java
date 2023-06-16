9f61ad90db6b08091c4405eb79285a791dafb340
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-16 12:13:10 +0530

    board

---
+++ java programming/Board.java
@@ -0,0 +1,21 @@
+class Board{
+ string color;
+ int price;
+ int weight;
+ int height;
+}
+class Board type{
+	string board brand;
+	string color;
+	string board quality;
+}
+class white extend Board{
+	string quality;
+	int price;
+	string pen;
+}
+class black Board{
+	string quality;
+	string chalk;
+	int length;
+}
