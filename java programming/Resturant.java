edc1bfeb6268f8d3bc3316c28f3d8dc33d8e3bbf
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-16 13:45:43 +0530

    resturant

---
+++ java programming/Resturant.java
@@ -0,0 +1,20 @@
+class Resturant {
+string menu style;
+string restuarant name;
+long price;
+}
+class Fastfood extends Restaurant {
+int price;
+int speed;
+string menu qauntity;
+}
+class Cafe extends Restaurant {
+string seating option;
+int price;
+string quantity;
+}
+class Family extend Restuarant {
+double area;
+int price;
+string seating option;
+}
