b58e44b8e331ccd4aef2292600216e35101f9c28
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-16 11:31:53 +0530

    Inheritance class and attributes

---
+++ java programming/Fruit.java
@@ -0,0 +1,18 @@
+class  Fruit{
+ string color;
+ list<fruitstype>  fruitstype;
+ int price;
+ double weight;
+}
+
+class Fruitstype{
+string fruits brand;
+string color;
+string fruitsquality;
+}
+class Apple extend Fruit{
+string quality;
+int Price;
+double weight;
+}
+
