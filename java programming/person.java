1e9d966f743fe63e3154e4b5f6fc84150b4be9bd
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-16 12:50:31 +0530

    person

---
+++ java programming/person.java
@@ -0,0 +1,29 @@
+class Person {
+ string name;
+ int age;
+ int height;
+ string city;
+ }
+ class person type{
+ string learn;
+ string walk;
+ string eat;
+ }
+ class dancer extend person{
+ string  group name;
+ double dancing;
+ string academy name;
+ }
+ class singing extend person{
+ int band name;
+ double singing;
+ string voice;
+ }
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
