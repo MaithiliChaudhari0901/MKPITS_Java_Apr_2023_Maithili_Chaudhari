748b05bd4a827881407839b666e0a7f58fe6aba2
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-16 13:18:29 +0530

    bank account

---
+++ java programming/bank account.java
@@ -0,0 +1,22 @@
+class BankAccount{
+ string Acount name;
+ double balance;
+ string branch name;
+ }
+ class intrestAccount extends  BankAccount {
+ float interest;
+double deposit;
+double withdraw;
+}
+class CheckingAccount extends BankAccount {
+ long number;
+ double withdraw;
+ long transaction;
+ }
+ class savingAccount extend BankAccount{
+ floatRate;
+ double Deposit;
+ Double withdraw;
+ }
+ 
+ 
