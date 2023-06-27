77160b244d78ae27d22d815d700da47292d29883
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-27 17:17:10 +0530

    html tag

---
+++ HTML.CSS.JS/html/heading tag.java
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>heading tags in html</title>
+</head>
+<body>
+    <!-- heading tags are h1 to h6 -->
+    <h1>Manali</h1>
+    <h2>Manali</h2>
+    <h3>Manali</h3>
+    <h4>Manali</h4>
+    <h5>Manali</h5>
+    <h6>Manali</h6>
+    <hr>
+
+    <h2>Bold tag</h2>
+    <p><b>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth.</b></p>
+
+
+    <h2>Strong tag</h2>
+    <p><strong>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth.</p></strong>
+
+
+    <h2>Itlic tag</h2>
+    <p><i>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth.</i></p>
+
+    <h2>Empasized tag</h2>
+    <p><em>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth.</em></p>
+
+
+    <h2>delete tag</h2>
+   <p><del>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</del></p>
+
+
+    <h2>strike tag</h2>
+    <p><str>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</str></p>
+
+
+    <h2>self tag</h2>
+    <p><s>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</s></p>
+
+     <h2>Big tag</h2>
+     <p><b>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</b></p>
+
+
+    <h2>Small tag</h2>
+    <p><s>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</s></p>
+
+
+
+
+    <h2>Mork tag</h2>
+    <p><m>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</s></p>
+
+
+
+    <h2>Insert tag</h2>
+    <p><ins>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</inc></p>
+
+
+
+
+
+   <h2>preserve tag</h2>
+   <p><pre>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</pre></p>
+
+    
+    
+    
+   <h2>BDO tag</h2>
+   <p><BDO>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</BDO></p>
+    
+    
+    
+    
+    <h2>short quotation tag</h2>
+    <p><sq>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</sq></p>
+
+
+    
+    <h2>Blockquote tag</h2>
+    <p><bck>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</bck></p>
+
+
+
+    
+    <h2>quatation tag</h2>
+    <p><qut>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</qut></p>
+
+
+
+
+
+    
+    <h2>Superscript tag</h2> 
+    <p><sup>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</qut></p>
+    
+    
+    
+    
+    <h2>subscript tag</h2>
+    <p><sub>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</sub></p>
+    
+    
+    
+    <h2>Adress tag</h2>
+    <p><ads>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</ads></p>
+    
+
+
+    
+    <h2>cite tag</h2>
+    <p><ci>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</ci></p>
+    
+
+
+    
+    <h2>centre tag</h2>
+    <p><ctr>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</ctr></p>
+
+
+    
+    <h2>Monospaced tag</h2>
+    <p><mono>education is the tool that break down all barriers. education is the part of the foundation of all progress and growth</mono></p>
+
+
+
+
+</body>
+  
+
+</html>
