6c992fea3e9430fabe3a9b8e6e386590402eaa5f
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-28 13:06:53 +0530

    data types

---
+++ java programming/Integer.java
@@ -0,0 +1,10 @@
+Class  Integer {
+public int method(int number2) {
+return number2;
+}
+ public static void main (String[] args) {
+ Integer data=new Integer();
+ int range=data.method(354678);
+ System.out.println("integer number is valid" +range);
+ }
+ }
