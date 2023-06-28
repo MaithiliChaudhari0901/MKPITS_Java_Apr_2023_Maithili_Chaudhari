838747e8e3dc708e2a036b23be7ff287fa9a147b
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-06-28 14:30:08 +0530

    data type

---
+++ java programming/Short.java
@@ -0,0 +1,10 @@
+class  Short{
+public short method(short number2) {
+return number2;
+}
+ public static void main (String[] args) {
+ Short data=new Short();
+ short range=data.method(356);
+ System.out.println("short number is valid" +range);
+ }
+ }
