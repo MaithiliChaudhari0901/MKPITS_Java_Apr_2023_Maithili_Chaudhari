9c683c7a62642aacc99330277b197e3807724855
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-08-08 18:14:54 +0530

    project 1

---
+++ java programming/Shape Interface/shapes/Circle.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public  class Circle implements Shape {
+    private int radius;
+
+
+
+
+    public Circle ( int radius) {
+        this.radius=radius;
+
+
+    }
+    public void area() {
+    System.out.println("area of circle :"+(3.14*radius*radius));
+
+        }
+        }
+
