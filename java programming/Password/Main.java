fd42b8816bf8733928cf755071c71a0a2e1fc48b
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-08-16 18:26:00 +0530

    project3

---
+++ java programming/Password/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+        public class Main {
+
+            public static void main(String args[]) {
+                Scanner s = new Scanner(System.in);
+                System.out.println("Enter the password");
+
+                System.out.println("Enter the conform");
+                String p = "123";
+
+                System.out.println("Enter the password +p");
+            }
+        }
+
