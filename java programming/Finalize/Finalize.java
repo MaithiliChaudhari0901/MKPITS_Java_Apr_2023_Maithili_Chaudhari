024a9a78e429021be9704cfd68b1460ea89cdc28
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-08-16 18:21:44 +0530

    project2

---
+++ java programming/Finalize/Finalize.java
@@ -0,0 +1,17 @@
+public class Finalize {
+    public static void main(String[] args) {
+        Finalize test = new Finalize();
+        String str = "finalize() method in Java";
+        str = null;
+        System.out.println(str);
+
+    }
+}
+    public static void main(String[] args) {
+    int marks=90;
+    }
+System.out.println(String.for(" my marks : %10.20f", marks));
+
+        System.out.println(String.for("|%-10d.20f",marks));//left forward minus justify //
+        }
+        }
---
+++ java programming/Finalize/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+        public static void sum(int... num) {
+            int s = 0;
+            for (int i : num) {
+                s = s + i;
+            }
+            return s;
+        }
+
+Sout
