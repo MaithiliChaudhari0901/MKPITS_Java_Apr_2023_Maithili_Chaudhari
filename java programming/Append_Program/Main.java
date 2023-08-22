1f4f18d6b11dcbdb1012a71781a1a4e2d472e4a6
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-08-23 02:49:21 +0530

    basic program

---
+++ java programming/Append_Program/Main.java
@@ -0,0 +1,9 @@
+import stud.ReadWriteObject;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[])throws IOException, ClassNotFoundException{
+        ReadWriteObject.writeToFile("xyz.txt");
+    }
+}
