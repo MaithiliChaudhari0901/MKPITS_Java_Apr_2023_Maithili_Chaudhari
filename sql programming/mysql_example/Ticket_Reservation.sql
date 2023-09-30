095ca88d8559e5506e4beca5a00d624e390f8085
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-09-30 18:45:34 +0530

    example of staff retrieve

--- sql programming/mysql_example/Ticket_Reservation.sql
+++ sql programming/mysql_example/Ticket_Reservation.sql
@@ -1,5 +1,6 @@
-use ticket_reservation
-select * from train
-select *  from reservation_status
-select * from Train where train_id='2'
-update train set source_='Wardha' where train_id='3'
+
+
+
+
+
+
