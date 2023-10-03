78083ed8a92ccbc3bfae2af24b48541b9067bb80
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-03 17:20:28 +0530

    retreive the recent rental

---
+++ sql programming/mysql_example/Retreive_recent_rental.sql
@@ -0,0 +1,9 @@
+use sakila
+show tables
+-- Retrieve the Customer from a specific city
+select  * from Customer_list
+
+-- Retrieve the most recent rentals
+select* from rental 
+select* from rental order by return_date desc
+
