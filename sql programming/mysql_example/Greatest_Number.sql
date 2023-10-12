2e44009de9a0e89ad2713f52f7f2af3797abe1e4
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-12 14:46:14 +0530

    example of greatest number

---
+++ sql programming/mysql_example/Greatest_Number.sql
@@ -0,0 +1,27 @@
+use sakila
+declare
+delimiter //
+create Procedure GreatestNumber(a int, b int , c int)
+begin
+
+if a>b and a>c then
+select "greatest number is a"+a;
+elseif b>a and b>c then
+select "greatest number is b"+b;
+else
+select "grreatest number is c"+c;
+end if;
+
+end
+//
+delimiter ;
+CALL GreatestNumber(34,23,67)
+
+  
+  
+ 
+      
+  
+  
+  
+ 
