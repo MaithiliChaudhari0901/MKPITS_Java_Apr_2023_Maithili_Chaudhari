59f1c7bffa63ec77aa9cff95e9476c663a655f49
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-12 10:48:45 +0530

    display of film title

---
+++ sql programming/mysql_example/Customer_id 1000.sql
@@ -0,0 +1,14 @@
+
+-- (38) Display the film titles rented by the customer with customer_id 1000.
+ use sakila
+ show tables
+select*from film
+select* from inventory
+select*from rental
+select* from customer
+
+select title from film join inventory  
+ on inventory.film_id= film.film_id join rental
+ on  rental.inventory_id=inventory.inventory_id join customer_id 1000.
+    
+    
