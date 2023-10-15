7ab85691208837b7d555446700ac185955dec970
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-15 19:33:32 +0530

    example of customer david clark

---
+++ sql programming/mysql_example/Retreive_Film_Title_Customer_id 1200.sql
@@ -0,0 +1,14 @@
+ -- (48) Retrieve the film titles and rental dates for all rentals made by the customer with customer_id 1200. 
+ 
+ use sakila
+ show tables
+    select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+    
+select title,rental_date from film join inventory
+ on inventory.film_id=film.film_id join rental
+ on rental.inventory_id=inventory.inventory_id join customer 
+ on customer.customer_id=rental.customer_id  where customer.customer_id=1200
+    
