5c0b040e8f34af1689f43723c5f5144420be23c1
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-20 22:53:47 +0530

    Example of HashMap collection

---
+++ java programming/Collection_HashMap2.java
@@ -0,0 +1,15 @@
+package coll;
+import java.util.*;
+public class Collection_HashMap2 {
+        public static void main(String args[]){
+
+            HashMap<Integer,String> hash_map= new HashMap<Integer,String>();
+            hash_map.put(1, "Prajacta Mohod");
+            hash_map.put(2, "Tanu Bodkhe");
+            hash_map.put(3, "Rutu Kadu");
+            hash_map.put(4, "Yashu Meshram ");
+            hash_map.put(5, "Prajkta Mohod");
+            System.out.println("Size of the hash map: "+hash_map.size());
+        }
+    }
+
