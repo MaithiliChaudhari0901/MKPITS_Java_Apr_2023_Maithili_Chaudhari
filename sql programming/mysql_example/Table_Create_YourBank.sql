bcdd20aaf2c5325850dd8ed3bbe3188209b37978
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-26 22:55:44 +0530

    example of create table of your bank

---
+++ sql programming/mysql_example/Table_Create_YourBank.sql
@@ -0,0 +1,11 @@
+ create database Your_Bank;
+ create  table YourAccount (
+ bank_userid   char  (200),
+  bank_password char (100),
+  bank_name char (60),
+  bank_address char (80),
+  bank_city char (60),
+  bank_balance  int
+  );
+ 
+ 
