543f78fea4ab96ae587f661f11698f0df218f40f
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-27 23:30:46 +0530

    examle of array create mynumber

---
+++ java programming/Array_MyNumber.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> MyNumber = new ArrayList<String>() ;
+
+                MyNumber.add("20");
+                MyNumber.add("50");
+                MyNumber.add("40");
+                MyNumber.add("63");
+                MyNumber.add("55");
+                for (String num: MyNumber) {
+                    System.out.println(num);
+                }
+            }
+        }
+
+
