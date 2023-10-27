7f706bfc3052a2d0ef3bd8ecf03932d054ca2a19
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-10-27 23:25:57 +0530

    example of create array

---
+++ java programming/Create_Array.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> Name = new ArrayList<String>() ;
+
+        Name.add("Sakshi");
+Name.add("Mahima");
+Name.add("Prisha");
+Name.add("Priya");
+Name.add("aayushi");
+for (String i: Name) {
+    System.out.println(i);
+}
+    }
+      }
