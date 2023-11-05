841653f168d10b8d962f2ed687ab63e87515fc3a
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-11-05 22:47:50 +0530

    example of thread

---
+++ java programming/Thread_Program.java
@@ -0,0 +1,14 @@
+public class Main extends Thread {
+    public static void main(String[] args) {
+        Main thread = new Main();
+                thread.start();
+                System.out.println("Welcome to my home");
+            }
+            public void run() {
+                System.out.println("Welcome to my college");
+
+            }
+        }
+
+
+
