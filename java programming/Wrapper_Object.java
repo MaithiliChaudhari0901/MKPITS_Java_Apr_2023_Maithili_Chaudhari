b2b42806e2995d2d90c0e3d46475260ab5c1f798
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-11-05 22:55:29 +0530

    example of wrapper object

---
+++ java programming/Wrapper_Object.java
@@ -0,0 +1,14 @@
+public class Wrapper_Object {
+
+        public static void main(String[] args) {
+            String Name = "maithili";
+            System.out.println("maithili");
+
+            Character Apple;
+            System.out.println("Apple");
+
+            Double number;
+            System.out.println("number");
+        }
+    }
+
