b4a55f95964cc54a4ae26c0de4caabfed8e5ae59
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-11-28 19:13:24 +0530

    example of array

---
+++ java programming/ArrayList_Program1/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Createing a list
+                List<String> list_Strings = new ArrayList<String>();
+
+                list_Strings.add("manu");
+                list_Strings.add("maithu");
+                list_Strings.add("aanu");
+                list_Strings.add("sanu");
+                list_Strings.add("Samu");
+
+                // Print the list
+
+                for (String element : list_Strings) {
+                    System.out.println(element);
+                }
+            }
+        }
+
+
