a9dfd33dac4e52cdff7e519065b2e00efc627fd6
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-11-29 16:02:55 +0530

    example of create table

---
+++ sql programming/mysql_example/Create_Table_StudentDetail.sql
@@ -0,0 +1,8 @@
+use college;
+drop  table StudentDetails; 
+CREATE TABLE  StudentDetails(Rollno int not null auto_increment,
+name varchar(55),
+Address varchar (55),
+city varchar (55),
+primary key(Rollno)
+);
