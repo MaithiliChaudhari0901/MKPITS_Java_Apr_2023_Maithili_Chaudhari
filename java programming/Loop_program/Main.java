3753790638115c1b7db0644f5a4ddb2a0916bb3e
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-12-04 11:00:48 +0530

    example of loop

---
+++ java programming/Loop_program/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int arr[]={12,23,44,56,78};
+//Printing array using for-each loop
+        for(int i:arr){
+            System.out.println(i);
+        }
+
+        }
+    }
+
+
+
