2b4d237a7cfcf530de6c27397e0d2932349ff851
maithili chaudhari <maithilichaudhari51@gmail.com>
2023-12-07 23:52:43 +0530

    example of cursor

---
+++ sql programming/mysql_example/SQL_Cursor_Program.sql
@@ -0,0 +1,34 @@
+use sakila
+drop table customer_one
+create table your_bank(
+name varchar(500),
+city varchar(500)
+);
+
+
+drop procedure Retrieve_Customer_Names_Addresses;
+delimiter //
+create procedure Retrieve_Customer_Names_Addresses()
+begin
+declare record varchar(500);
+declare namee,cityy varchar(500);
+declare cursor1 cursor for
+select customer.first_name,address.address from customer join address on
+customer.address_id=address.address_id;
+declare continue handler for not found set record =1;
+open cursor1;
+label:loop
+fetch cursor1 into namee ,cityy ;
+insert into customer_one value (namee ,cityy);
+if record=1 then
+leave label;
+end if;
+end loop;
+close cursor1;
+end
+// delimiter ;
+
+
+call Retrieve_Customer_Names_Addresses
+select *from customer_one
+
